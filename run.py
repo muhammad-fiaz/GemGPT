132e5fa3d097e02dc532b21340ab02dc92d912ed
muhammad-fiaz <contact@muhammadfiaz.com>
2024-03-29 21:33:25 +0530

    initial commit

--- run.py
+++ run.py
@@ -1,7 +1,8 @@
+import gradio as gr
 from transformers import AutoTokenizer, pipeline
 import torch
 
-model = "google/gemma-7b-it"
+model = "google/gemma-2b-it"
 
 tokenizer = AutoTokenizer.from_pretrained(model)
 pipeline = pipeline(
@@ -13,17 +14,36 @@ pipeline = pipeline(
     device="cuda",
 )
 
-while True:
-    user_input = input("You: ")
-    messages = [{"role": "user", "content": user_input}]
+
+def chatbot(input_text, max_new_tokens=1250, temperature=0.7, top_k=50, top_p=0.95):
+    messages = [{"role": "user", "content": input_text}]
     prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
     outputs = pipeline(
         prompt,
-        max_new_tokens=256,
+        max_new_tokens=max_new_tokens,
         do_sample=True,
-        temperature=0.7,
-        top_k=50,
-        top_p=0.95
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p
     )
     generated_text = outputs[0]["generated_text"][len(prompt):]
-    print("Bot:", generated_text)
+    return generated_text
+
+
+
+
+iface = gr.Interface(
+    fn=chatbot,
+    inputs=[
+        gr.Textbox(lines=2, label="Input"),
+        gr.Slider(minimum=50, maximum=2000, label="Max New Tokens",  value=1250),
+        gr.Slider(minimum=0.0, maximum=1.0, label="Temperature",  value=0.7),
+        gr.Slider(minimum=1, maximum=100, label="Top K",  value=50),
+        gr.Slider(minimum=0.0, maximum=1.0, label="Top P",  value=0.95),
+    ],
+    outputs=gr.Textbox(label="Output"),
+    title="GemGPT",
+    description="Talk to GemGPT,Powered by a 2 billion parameter Gemma Model",
+)
+
+iface.launch()
