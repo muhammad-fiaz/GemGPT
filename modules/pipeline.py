7f245f178cf3144adfbd8ab4eed131b5d7b81946
muhammad-fiaz <contact@muhammadfiaz.com>
2024-05-15 17:11:47 +0530

    improved UI and Added 2bit quantization

--- modules/pipeline.py
+++ modules/pipeline.py
@@ -3,10 +3,12 @@ import os
 from modules.log import logly
 
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ['SEGMENT_ANYTHING_FAST_USE_FLASH_4'] = '0'
+os.environ['USE_FLASH_ATTENTION'] = '1'
+
 import torch
 from transformers import pipeline, AutoTokenizer, BitsAndBytesConfig
-
-
+chat_history = []
 def pipeline_tunnel(model, device):
     return pipeline(
         "text-generation",
@@ -19,11 +21,12 @@ def pipeline_tunnel(model, device):
 
 
 def chatbot(input_text, max_new_tokens=1250, temperature=0.7, top_k=50, top_p=0.95, model="google/gemma-2b-it",
-            quantization="4-bit", device="cuda"):
+            quantization="2-bit", device="cuda"):
     try:
-        if quantization == "4-bit":
+        if quantization == "2-bit":
+            quantization_config = BitsAndBytesConfig(load_in_2bit=True)
+        elif quantization == "4-bit":
             quantization_config = BitsAndBytesConfig(load_in_4bit=True)
-
         else:
             quantization_config = BitsAndBytesConfig(load_in_8bit=True)
 
@@ -41,7 +44,8 @@ def chatbot(input_text, max_new_tokens=1250, temperature=0.7, top_k=50, top_p=0.
             top_p=top_p
         )
         generated_text = outputs[0]["generated_text"][len(prompt):]
-        return generated_text
+        chat_history.append((input_text, generated_text))
+        return chat_history
     except Exception as e:
         logly.error(f"Error in GemGPT: {e}")
         return str(e)
